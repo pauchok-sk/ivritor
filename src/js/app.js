d321492ea04667ee981e7259a0a74134178bb3c4
pauchok-sk <pauchoooooook@gmail.com>
2025-08-04 10:00:17 +0300

    master

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+import "../scss/style.scss";
+import anchors from "./files/anchors.js";
+import burger from "./files/burger.js";
+import spoller from "./files/spoller.js";
+
+spoller();
+burger();
+anchors();
